5739889ee6efb81ec51aad251fac15be2b771755
raunakadvani2410 <ra1113@georgetown.edu>
2024-08-05 00:27:52 -0400

    update main

--- main_login.py
+++ main_login.py
@@ -26,7 +26,7 @@ def enter_webpage(link):
     cd_path = Service('./chromedriver')
     # TODO where does this go??
     chrome_options = Options()
-    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
