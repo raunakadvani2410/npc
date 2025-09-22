52e7b821e53867173f5678c2059b851b5afadec3
raunakadvani2410 <ra1113@georgetown.edu>
2025-09-22 00:09:11 -0400

    turn on pyvirtualdisplay

--- main_login_ss.py
+++ main_login_ss.py
@@ -23,8 +23,8 @@ from pyvirtualdisplay import Display
 
 def enter_webpage(link):
     # store exe directory
-    # cd_path = Service('./chromedriver')
-    cd_path = Service('/user/bin/chromedriver')
+    cd_path = Service('./chromedriver')
+
     
     # set chrome options
     chrome_options = Options()
